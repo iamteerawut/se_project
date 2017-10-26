ab45023257be7ff1269df12a7e880f5c91e5da44
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-10-26 15:27:42 +0700

    Update ui with bottom tab

--- Pillminder/app/src/main/java/com/se/pillminder/MainActivity.java
+++ Pillminder/app/src/main/java/com/se/pillminder/MainActivity.java
@@ -1,13 +1,49 @@
 package com.se.pillminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    setTitle("Main Page");
+                    MainPage fragment = new MainPage();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, fragment, "FragmentName");
+                    return true;
+                case R.id.navigation_dashboard:
+                    setTitle("Alert Page");
+                    AlertPage fragment2 = new AlertPage();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content, fragment2, "FragmentName");
+                    return true;
+                case R.id.navigation_notifications:
+                    setTitle("Setting Page");
+                    SettingPage fragment3 = new SettingPage();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.content, fragment3, "FragmentName");
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
