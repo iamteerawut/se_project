d86dd3f6c298e879b419faf7f60436c77d033592
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-11-05 16:33:46 +0700

    Update icon nav and clean-up code

--- Pillminder/app/src/main/java/com/se/pillminder/PillPage.java
+++ Pillminder/app/src/main/java/com/se/pillminder/PillPage.java
@@ -1,29 +1,22 @@
 package com.se.pillminder;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PillPage extends Fragment {
-
 
     public PillPage() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_pill_page, container, false);
     }
 
