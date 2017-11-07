237fb129e883c34a11d4fd754f5cfc95de7c284d
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-11-07 16:50:36 +0700

    Add dialog edit pill

--- Pillminder/app/src/main/java/com/se/pillminder/PillFragment.java
+++ Pillminder/app/src/main/java/com/se/pillminder/PillFragment.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PillFragment extends Fragment implements View.OnClickListener {
+public class PillFragment extends Fragment {
 
     private View rootView;
 
@@ -37,26 +37,63 @@ public class PillFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         this.rootView = inflater.inflate(R.layout.fragment_pill, container, false);
 
-        Button add_pill = rootView.findViewById(R.id.add_button);
-        add_pill.setOnClickListener(this);
+
+        Button edit_pill = rootView.findViewById(R.id.edit_pill1);
+        edit_pill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(PillFragment.this.getContext());
+                dialog.setContentView(R.layout.dialog_editpill);
+
+                Button ok_action = (Button) dialog.findViewById(R.id.ok_action);
+                Button cancel_action = (Button) dialog.findViewById(R.id.cancel_action);
+
+                ok_action.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                cancel_action.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
+        Button add_pill = rootView.findViewById(R.id.add_pill);
+        add_pill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(PillFragment.this.getContext());
+                dialog.setContentView(R.layout.dialog_addpill);
+
+                Button ok_action = (Button) dialog.findViewById(R.id.ok_action);
+                Button cancel_action = (Button) dialog.findViewById(R.id.cancel_action);
+
+                ok_action.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                cancel_action.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
 
         return rootView;
 //        return inflater.inflate(R.layout.fragment_pill, container, false);
     }
-
-    @Override
-    public void onClick(View view) {
-        final Dialog dialog = new Dialog(PillFragment.this.getContext());
-        dialog.setContentView(R.layout.dialog_addpill);
-
-        Button cancel_action = (Button) dialog.findViewById(R.id.cancel_action);
-        cancel_action.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-    }
 }
+
