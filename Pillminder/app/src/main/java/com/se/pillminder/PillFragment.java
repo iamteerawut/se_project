a97989150cba339c0df6b118b4289d5f07e27984
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-11-12 13:45:14 +0700

    Update all ui

--- Pillminder/app/src/main/java/com/se/pillminder/PillFragment.java
+++ Pillminder/app/src/main/java/com/se/pillminder/PillFragment.java
@@ -37,32 +37,32 @@ public class PillFragment extends Fragment {
         // Inflate the layout for this fragment
         this.rootView = inflater.inflate(R.layout.fragment_pill, container, false);
 
-        Button edit_pill = rootView.findViewById(R.id.edit_pill1);
-        edit_pill.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Dialog dialog = new Dialog(PillFragment.this.getContext());
-                dialog.setContentView(R.layout.dialog_editpill);
-
-                Button ok_action = (Button) dialog.findViewById(R.id.ok_action);
-                Button cancel_action = (Button) dialog.findViewById(R.id.cancel_action);
-
-                ok_action.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-
-                cancel_action.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();
-            }
-        });
+//        Button edit_pill = rootView.findViewById(R.id.edit_pill1);
+//        edit_pill.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                final Dialog dialog = new Dialog(PillFragment.this.getContext());
+//                dialog.setContentView(R.layout.dialog_editpill);
+//
+//                Button ok_action = (Button) dialog.findViewById(R.id.ok_action);
+//                Button cancel_action = (Button) dialog.findViewById(R.id.cancel_action);
+//
+//                ok_action.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//
+//                cancel_action.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                dialog.show();
+//            }
+//        });
 
         Button add_pill = rootView.findViewById(R.id.add_pill);
         add_pill.setOnClickListener(new View.OnClickListener() {
