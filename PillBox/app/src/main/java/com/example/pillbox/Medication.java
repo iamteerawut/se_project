480b939229bc343077e43e09e729b46b53256232
iamteerawut <t.kitbunjerdjarud@gmail.com>
2017-12-10 23:19:11 +0700

    Something just like this

---
+++ PillBox/app/src/main/java/com/example/pillbox/Medication.java
@@ -0,0 +1,107 @@
+package com.example.pillbox;
+
+/**
+ * Created by luishengjie on 6/6/17.
+ */
+
+public class Medication {
+    private String medId;
+    private String medName;
+    private int dosage;
+    private int frequency;
+    private String beforeFood;
+    private int amount;
+    private String units;
+    private String startDate;
+    private String endDate;
+    private int morning = 0;
+    private int afternoon = 0;
+    private int evening = 0;
+    private int night = 0;
+
+
+
+    public Medication(String medId, String medName, int dosage, int morning, int afternoon,
+            int evening, int night, String beforeFood, int amount, String units){
+        this.medId = medId;
+        this.medName = medName;
+        this.dosage = dosage;
+        this.beforeFood = beforeFood;
+        this.amount = amount;
+        this.units = units;
+
+        this.morning = morning;
+        this.afternoon = afternoon;
+        this.evening = evening;
+        this.night = night;
+    }
+
+    public Medication(String medId, String medName, int dosage, int morning, int afternoon,
+                      int evening, int night, String beforeFood, int amount, String units, String startDate, String endDate){
+        this.medId = medId;
+        this.medName = medName;
+        this.dosage = dosage;
+        this.morning = morning;
+        this.afternoon = afternoon;
+        this.evening = evening;
+        this.night = night;
+        this.beforeFood = beforeFood;
+        this.amount = amount;
+        this.units = units;
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+    }
+
+
+
+    public String getMedId(){
+        return medId;
+    }
+
+    public String getMedName(){
+        return medName;
+    }
+
+    public int getDosage(){
+        return dosage;
+    }
+
+
+    public String getBeforeFood(){
+        return beforeFood;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public String getUnits(){
+        return units;
+    }
+
+    public String getStartDate(){
+        return startDate;
+    }
+
+    public String getEndDate(){
+        return endDate;
+    }
+
+    public  int getMorning(){
+        return morning;
+    }
+
+    public int getAfternoon(){
+        return afternoon;
+    }
+
+    public int getEvening(){
+        return evening;
+    }
+
+    public int getNight(){
+        return night;
+    }
+
+}
---
+++ PillBox/app/src/main/java/com/example/pillbox/model/ReportFragment.java
@@ -0,0 +1,43 @@
+package com.example.pillbox.model;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.pillbox.R;
+
+/**
+ * Created by luishengjie on 5/6/17.
+ */
+
+public class ReportFragment extends Fragment {
+    // This method creates a new instance of the PillBoxFragment
+    public static ReportFragment newInstance(){
+        ReportFragment fragment = new ReportFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_report, container, false);
+        ProgressBar _progressBar = (ProgressBar)rootView.findViewById (R.id.circularProgressBar);
+        _progressBar.setProgress( 30 );
+        ((TextView) rootView.findViewById(R.id.textView1)).setText("50%");
+
+
+        return rootView;
+    }
+}
+
+
